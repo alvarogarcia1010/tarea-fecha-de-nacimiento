76cc1c76cf58fb44923b195640990589e61a4bd7
alvarogarcia1010 <00111317@uca.edu.sv>
2018-10-26 22:23:37 -0600

    Terminado

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ var person = {
     /* property: value*/
     name: "Alvaro",
     lastname: "Garcia",
-    birthday: new Date(1998, 8, 10)
+    birthday: new Date(1998, 9, 29)
 }; // JSON (JavaScript Object Notation)
-
-// Access to propertys
-console.log(person.name);
-console.log(person.birthday);
-person.dui = "000000000" // Assign
 
 // Example
 function createPerson(name, lastname, birthday, dui) {
@@ -21,28 +16,17 @@ function createPerson(name, lastname, birthday, dui) {
     }
 }
 
-let list = [] // To save persons
-
-// To Add 10 fake persons
-for (let i = 0; i < 10; i++) {
-    list.push(createPerson(`Name ${i}`, `Lastname ${i}`, new Date().setFullYear(1990 + i + Math.floor(Math.random() * 5)), `000000${i}`));
-}
-
-//console.table(list);
-
 // Get average age
 function getAge(birthday) {
-    var ageDifMs = Date.now() - birthday;
-    console.log(ageDifMs);
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    var miliYear = 1000*60*60*24*365;
-    console.log(ageDate/miliYear);
+  var hoy = new Date();
+  var edad = hoy.getFullYear() - birthday.getFullYear();
+  var mes = hoy.getMonth() - birthday.getMonth();
 
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  if (mes < 0 || (mes == 0 && hoy.getDate() < birthday.getDate())) {
+      edad--;
+  }
+
+  return edad;
 }
 
 console.log(getAge(person.birthday))
-
-// console.log("Average age %i", list.reduce((sum, {
-//     birthday
-// }) => getAge(birthday) + sum, 0) / list.length);
